572aa0d40d1ea4a14d4cc9c28dd086045c0f21d4
dvirbenshlush <dvirbsh95@gmail.com>
2024-06-18 03:01:30 +0300

    paginagion

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/search/searchController');
 
-router.get('/', searchController.search);
+// router.post('/', (req, res) => {
+//     // Add your parameter here
+//     const parameter = req.body.currentPage;
+//     searchController.search(parameter);
+// });
+
+router.post('/', searchController.search);
+
 
 module.exports = router;
